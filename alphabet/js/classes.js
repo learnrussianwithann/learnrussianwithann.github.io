48fa17f27b40e306801c02d80f93309e9a3d4125
learnrussianwithann <vladimir.m.schegolev@yandex.ru>
2020-08-28 15:00:16 +0300

    relocate

---
+++ alphabet/js/classes.js
@@ -0,0 +1,361 @@
+class Point {
+	constructor(x, y) {
+		if (x == undefined) {
+			this.x = 0;
+			this.y = 0;
+		} else {
+			this.x = x;
+			if (y == undefined) {
+				this.y = x;
+			} else {
+				this.y = y;
+			}
+		}
+	}
+
+	set(x, y) {
+		if (x == undefined) {
+			this.x = 0;
+			this.y = 0;
+		} else {
+			this.x = x;
+			if (y == undefined) {
+				this.y = x;
+			} else {
+				this.y = y;
+			}
+		}
+	}
+
+	multiple(d) {
+		this.x *= d;
+		this.y *= d;
+		return this;
+	}
+
+	static subtractPoint(a, b) {
+		return new Point(a.x - b.x, a.y - b.y);
+	}
+
+	sumPoint(d) {
+		this.x += d.x;
+		this.y += d.y;
+		return this;
+	}
+
+	dist(x, y) {
+		return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+	}
+
+	distToPoint(point) {
+		return this.dist(point.x, point.y);
+	}
+
+	static getVector(a, b) {
+		return new Point(b.x - a.x, b.y - a.y);
+	}
+
+	static getPoint(elem) {
+		return new Point(elem.x, elem.y);
+	}
+
+	toString() {
+		return 'x: ' + this.x + ' y: ' + this.y;
+	}
+}
+
+class Element extends PIXI.Container {
+	constructor(name) {
+		super();
+		this.interactiveChildren = false;
+		this.ratio = 1;
+		if (name) this.name = name;
+	}
+
+	add(elem) {
+		this.addChild(elem);
+		this.updateHitArea();
+	}
+
+	updateHitArea() {
+		this.calculateBounds();
+		this.hitArea = this.getLocalBounds();
+	}
+
+	getByName(name) {
+		return this.getChildByName(name);
+	}
+
+	hide(name) {
+		this.getByName(name).visible = false;
+	}
+
+	show(name) {
+		this.getByName(name).visible = true;
+	}
+}
+
+class MaskText extends Element {
+	constructor(name, texture, textStyle) {
+		super(name);
+		this.add(texture);
+		this.text = new PIXI.Text('', textStyle);
+		this.text.anchor.set(.5);
+		this.add(this.text);
+		this.mask = this.text;
+		this.gender = '';
+	}
+
+	setText(word) {
+		this.text.text = word.word;
+		this.gender = word.gender;
+	}
+}
+
+class Viewport {
+	constructor(application, ratio) { //ratio width/height
+		this.app = application;
+		this.container = new PIXI.Container();
+		this.w = 100;
+		this.h = 100;
+		this.c = {x:0, y:0};
+		if (ratio) this.ratio = ratio;
+		else this.ratio = 16 / 9;
+		this.app.stage.addChild(this.container);
+	}
+
+	add(elem, x, y, w, byHeight) {
+		elem.info = { x: x, y: y, size: w, byHeight: byHeight };
+		this.container.addChild(elem);
+	}
+
+	removeAll() {
+		this.container.removeChildren(0, this.container.children.lenght);
+	}
+
+	remove(elem) {
+		this.container.removeChild(elem);
+	}
+
+	getContainer() {
+		return this.container;
+	}
+
+	resizeElement(e) {
+		e.calculateBounds();
+		e.scale.set(1);
+		e.ratio = e.width / e.height;
+		if (e.info.byHeight) {
+			e.height = this.w * e.info.size;
+			e.width = e.height * e.ratio;
+			e.x = this.w * e.info.x + this.c.x;
+			e.y = this.h * e.info.y + this.c.y;
+		} else {
+			e.width = this.w * e.info.size;
+			e.height = e.width / e.ratio;
+			e.x = this.w * e.info.x + this.c.x;
+			e.y = this.h * e.info.y + this.c.y;
+		}
+	}
+
+	resize() {
+		this.w = this.app.screen.width;
+		this.h = this.app.screen.height;
+		this.c.set(this.w / 2, this.h / 2);
+		if (this.w / this.ratio < this.h) {
+			this.h = this.w / this.ratio;
+		} else {
+			this.w = this.h * this.ratio;
+		}
+		this.c.x -= this.w / 2;
+		this.c.y -= this.h / 2;
+		this.container.children.forEach(element => {
+			this.resizeElement(element);
+		});
+	}
+
+	show() {
+		this.container.visible = true;
+	}
+
+	hide() {
+		this.container.visible = false;
+	}
+}
+
+class Animator {
+	constructor(application, fps) {
+		this.application = application;
+		window.animator = this;
+		this.animations = { next: null };
+		this.isRun = false;
+		if (fps == null) this.fps = 60;
+		else this.fps = fps;
+		this.endFunc = null;
+		this.jumping = [];
+	}
+
+	fmove(element, arg, steps) {
+		element.x += arg.x * steps;
+		element.y += arg.y * steps;
+	}
+
+	falpha(element, arg, steps) {
+		element.alpha += arg * steps;
+	}
+
+	fscale(element, arg, steps) {
+		element.scale.x += arg.x * steps;
+		element.scale.y += arg.y * steps;
+	}
+
+	frotate(element, arg, steps) {
+		element.rotation += arg * steps;
+	}
+
+	addNewAnimationMove(elem, start, end, time, endFunc) {
+		if (start == null) start = new Point(elem.x, elem.y);
+		let steps = Math.ceil(time * this.fps);
+		let step = Point.subtractPoint(end, start).multiple(1 / steps);
+		elem.position.set(start.x, start.y);
+		new EAnimation(this.animations, elem, this.fmove, steps, step, endFunc);
+		this.start();
+	}
+
+	addNewAnimationAlpha(elem, start, end, time, endFunc) {
+		let steps = Math.ceil(time * this.fps);
+		let disp = (end - start) / steps;
+		elem.alpha = start;
+		new EAnimation(this.animations, elem, this.falpha, steps, disp, endFunc);
+		this.start();
+	}
+
+	addNewAnimationScale(elem, startScale, endScale, baseScale, time, endFunc) {
+		if (startScale == null) startScale = elem.scale;
+		else {
+			if (!startScale.hasOwnProperty('x')) startScale = { x: startScale, y: startScale };
+			if (baseScale != null) {
+				startScale.x *= baseScale.x;
+				startScale.y *= baseScale.y;
+			}
+			elem.scale.set(startScale.x, startScale.y);
+		}
+		if (!endScale.hasOwnProperty('x')) endScale = { x: endScale, y: endScale };
+
+		if (baseScale != null) {
+			endScale.x *= baseScale.x;
+			endScale.y *= baseScale.y;
+		}
+
+		let steps = Math.ceil(time * this.fps);
+		let step = Point.subtractPoint(endScale, startScale).multiple(1 / steps);
+		new EAnimation(this.animations, elem, this.fscale, steps, step, endFunc);
+		this.start();
+	}
+
+	addNewAnimationRotation(elem, startAngle, endAngle, time, endFunc) {
+		if (startAngle == null) startAngle = elem.rotation;
+		let steps = Math.ceil(time * this.fps);
+		let step = (endAngle - startAngle) / steps;
+		if (elem.rotation != startAngle) elem.rotation == startAngle;
+		new EAnimation(this.animations, elem, this.frotate, steps, step, endFunc);
+		this.start();
+	}
+
+	tick() {
+		if (this.animations.next == null) this.stop();
+		else this.animations.next.do();
+		this.application.render();
+	}
+
+	start() {
+		if (!this.isRun) {
+			this.isRun = true;
+			this.timerId = setInterval(function () { window.animator.tick() }, 1000 / this.fps);
+		}
+	}
+
+	stop() {
+		this.isRun = false;
+		clearInterval(this.timerId);
+		if (this.endFunc != null) this.endFunc();
+		this.endFunc = null;
+	}
+
+	addAnimationJump(elem) {
+		if (!this.jumping.includes(elem)) {
+			let jumping = this.jumping;
+			jumping.push(elem);
+			let tprep = 0.1;
+			let tup = .1;
+			let tdown = .1;
+			let ttonormal = .1;
+			let h = elem.getLocalBounds().height;
+			let fMove = this.addNewAnimationMove.bind(this);
+			setTimeout(function () {
+				fMove(elem, null, { x: 0, y: -.05 * h }, tup, () => {
+					fMove(elem, null, { x: 0, y: .01 * h }, tdown + tprep, () => {
+						fMove(elem, null, { x: 0, y: 0 }, ttonormal, () => { jumping.splice(jumping.indexOf(elem), 1); });
+					});
+				});
+			}, tprep * 1000);
+
+			let fScale = this.addNewAnimationScale.bind(this);
+			let baseScale = elem.scale.clone();
+			fScale(elem, null, { x: 1.05, y: .95 }, baseScale, tprep, () => {
+				fScale(elem, null, { x: .95, y: 1.05 }, baseScale, tup, () => {
+					fScale(elem, null, 1, baseScale, tdown);
+				})
+			})
+		}
+	}
+
+	doAll() {
+		while (this.animations.next != null) {
+			this.animations.next.doAll();
+		}
+	}
+}
+
+
+
+class EAnimation {
+	constructor(prev, element, func, steps, arg, endFunc) {
+		this.prev = prev;
+		if (prev.next) {
+			prev.next.prev = this;
+			this.next = prev.next;
+		}
+		prev.next = this;
+		this.count = steps;
+		this.element = element;
+		this.func = func;
+		this.arg = arg
+		this.endFunc = endFunc;
+	}
+
+	do() {
+		if (this.count > 0) {
+			this.func(this.element, this.arg, 1);
+			this.count--;
+		} else {
+			this.end();
+		}
+		if (this.next != null) this.next.do();
+	}
+
+	doAll() {
+		this.func(this.element, this.arg, this.count);
+		this.count = -1;
+		this.end();
+		if (this.next != null) this.next.doAll();
+	}
+
+	end() {
+		if (this.next) this.next.prev = this.prev;
+		this.prev.next = this.next;
+		if (this.endFunc != null) {
+			this.endFunc();
+		}
+	}
+}
---
+++ alphabet/js/support.js
@@ -0,0 +1,334 @@
+const wordsF = [
+	'морковь',
+	'пыль',
+	'лошадь',
+	'кровать',
+	'зелень',
+	'дверь',
+	'игра',
+	'пила',
+	'картина',
+	'туча',
+	'погода',
+	'ягода',
+	'голова',
+	'мечта',
+	'мысль',
+	'речь',
+	'ночь',
+	'дочь',
+	'ручка',
+	'причёска',
+	'юбка',
+	'футболка',
+	'бабушка',
+	'сестра',
+	'семья',
+	'вилка',
+	'сказка',
+	'книга',
+	'школа',
+	'улыбка',
+	'песня',
+	'музыка',
+	'каша',
+	'фея',
+	'комната',
+	'подруга',
+	'тень',
+	'помощь',
+	'дочь',
+	'вещь',
+]
+const wordsN = [
+	'поле',
+	'море',
+	'пальто',
+	'радио',
+	'кино',
+	'кафе',
+	'варенье',
+	'небо',
+	'одеяло',
+	'зеркало',
+	'стекло',
+	'слово',
+	'дупло',
+	'плечо',
+	'молоко ',
+	'метро',
+	'кимоно',
+	'пюре',
+	'пианино',
+	'блюдо',
+	'шоссе',
+	'желе',
+	'меню',
+	'яблоко',
+	'ухо',
+	'эхо',
+	'дерево',
+	'колено',
+	'лето',
+	'письмо',
+	'зерно',
+	'гнездо',
+	'платье',
+	'солнце',
+	'полотенце',
+	'время',
+	'племя',
+	'пламя',
+	'чтение',
+	'отражение',
+]
+const wordsM = [
+	'папа',
+	'дядя',
+	'друг',
+	'брат',
+	'урок',
+	'луч',
+	'меч',
+	'заяц',
+	'куст',
+	'лес',
+	'крик',
+	'сон',
+	'глаз',
+	'стол',
+	'карандаш',
+	'лист',
+	'вес',
+	'магазин',
+	'поход',
+	'дождь',
+	'день',
+	'ремень',
+	'огонь',
+	'шампунь',
+	'голубь',
+	'конь',
+	'мост',
+	'экран',
+	'герой',
+	'рассказ',
+	'воздух',
+	'ключ',
+	'медведь',
+	'снег',
+	'тюлень',
+	'мотоцикл',
+	'фонтан',
+	'путь',
+	'мужчина',
+	'ботинок',
+]
+
+var indexN = Math.floor(Math.random() * wordsN.length)
+var indexF = Math.floor(Math.random() * wordsF.length)
+var indexM = Math.floor(Math.random() * wordsM.length)
+
+function getDrawRect(w, h, r, color) {
+	let out = new PIXI.Graphics()
+	if (color) out.beginFill(color)
+	else out.beginFill(0xff6868)
+	out.drawRoundedRect(-w / 2, -h / 2, w, h, r)
+	out.endFill()
+	return out
+}
+
+function dist(x1, y1, x2, y2) {
+	return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
+}
+
+function distElement(a, b) {
+	return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
+}
+
+function distToMouse(word, mouse) {
+	return (
+		Math.abs(mouse.x - word.x) < word.width / 2 &&
+		Math.abs(mouse.y - word.y) < word.height
+	)
+}
+
+// function onDragStart(event) {
+// 	// store a reference to the data
+// 	// the reason for this is because of multitouch
+// 	// we want to track the movement of this particular touch
+// 	this.data = event.data;
+// 	// this.alpha = 0.5;
+// 	this.dragging = true;
+// 	this['offset'] = this.data.getLocalPosition(this.parent);
+// 	this.offset.set(this.x - this.offset.x, this.y - this.offset.y);
+// }
+
+// function onDragEnd() {
+// 	this.dragging = false;
+// 	// set the interaction data to null
+// 	this.data = null;
+// 	let max_dist = 0.1 * viewGame.h;
+// 	if (distToMouse(this, window.mouseF)) {
+// 		if (curWordGender == 'f') correct(window.mouseF);
+// 		else incorrect(window.mouseF);
+// 	} else if (distToMouse(this, window.mouseN)) {
+// 		if (curWordGender == 'n') correct(window.mouseN);
+// 		else incorrect(window.mouseN);
+// 	} else if (distToMouse(this, window.mouseM)) {
+// 		if (curWordGender == 'm') correct(window.mouseM);
+// 		else incorrect(window.mouseM);
+// 	} else if (distToMouse(this, window.cat)) {
+// 		window.catMeow();
+// 	}
+// }
+
+// function onDragMove() {
+// 	if (this.dragging) {
+// 		const newPosition = this.data.getLocalPosition(this.parent);
+// 		let x = newPosition.x + this.offset.x;
+// 		let y = newPosition.y + this.offset.y;
+// 		if (x > 50 && y > 10 && x < app.screen.width - 50 && y < app.screen.height - 10) {
+// 			this.x = newPosition.x + this.offset.x;
+// 			this.y = newPosition.y + this.offset.y;
+// 		}
+// 	}
+// }
+
+function writeNewWord(text) {
+	text.text = getRandom()
+}
+
+function getWordF() {
+	if (++indexF >= wordsF.length) indexF = 0
+	return wordsF[indexF]
+}
+
+function getWordN() {
+	if (++indexN >= wordsN.length) indexN = 0
+	return wordsN[indexN]
+}
+
+function getWordM() {
+	if (++indexM >= wordsM.length) indexM = 0
+	return wordsM[indexM]
+}
+
+function genWords() {
+	let out = []
+	let countf = 0,
+		countn = 0,
+		countm = 0;
+	while (countf < 4 || countm < 4 || countn < 4) {
+		switch (Math.floor(Math.random() * 3)) {
+			case 0:
+				if (countf < 4) {
+					countf++;
+					out.push({word:getWordF(), gender:'f'});
+				}
+				break
+			case 1:
+				if (countn < 4) {
+					countn++;
+					out.push({word:getWordN(), gender:'n'});
+				}
+				break
+			case 2:
+				if (countm < 4) {
+					countm++;
+					out.push({word:getWordM(), gender:'m'});
+				}
+				break
+		}
+	}
+	return out;
+}
+
+function genCloud(texture, text, style, anchor, scale = 3.6) {
+	let cloud = new PIXI.Container()
+	cloud.name = 'cloud'
+	cloud.addChild(genSprite(texture, 'cloud', { x: -0.3, y: 0.5 }, scale))
+	let cloudName = new PIXI.Text(text, style)
+	cloudName.anchor.set(anchor.x, anchor.y)
+	cloud.addChild(cloudName)
+	cloud.alpha = 0;
+	cloud.zIndex = 10;
+	cloud.getChildByName('cloud').anchor.set(-.4,1);
+	return cloud
+}
+
+function genSprite(texture, name, anchor, scale, position) {
+	let out = new PIXI.Sprite(texture)
+	out.name = name
+	if (anchor != null) {
+		if (anchor.hasOwnProperty('x')) {
+			out.anchor.x = anchor.x
+			out.anchor.y = anchor.y
+		} else out.anchor.set(anchor)
+	}
+	if (scale != null) {
+		if (scale.hasOwnProperty('x')) {
+			out.scale.x = scale.x
+			out.scale.y = scale.y
+		} else out.scale.set(scale)
+	}
+	if (position != null) {
+		out.x = position.x
+		out.y = position.y
+	}
+	return out
+}
+
+function genButton(text, style, bcolor, k1 = 1.2, k2 = 2) {
+	let out = new Element()
+	let t = new PIXI.Text(text, style)
+	t.anchor.set(0.5)
+
+	out.add(getDrawRect(k1 * t.width, k2 * t.height, k2 * t.height, bcolor))
+	out.add(t)
+	return out
+}
+
+function getRandom() {
+	switch (Math.floor(Math.random() * 3)) {
+		case 0:
+			if (++indexF >= wordsF.length) indexF = 0
+			curWordGender = 'f'
+			return wordsF[indexF]
+		case 1:
+			if (++indexN >= wordsN.length) indexN = 0
+			curWordGender = 'n'
+			return wordsN[indexN]
+		case 2:
+			if (++indexM >= wordsM.length) indexM = 0
+			curWordGender = 'm'
+			return wordsM[indexM]
+	}
+}
+
+function setMoveable(
+	element,
+	onDragStart,
+	onDragMove,
+	onDragEnd,
+	updateFunction,
+) {
+	element
+		.on('pointerdown', onDragStart)
+		.on('pointerdown', updateFunction)
+		.on('pointerup', onDragEnd)
+		.on('pointerup', updateFunction)
+		.on('pointerupoutside', onDragEnd)
+		.on('pointerupoutside', updateFunction)
+		.on('pointermove', onDragMove)
+		.on('pointermove', updateFunction)
+
+	element.interactive = true
+	element.buttonMode = true
+}
+
+function setButton(element, event) {
+	element.on('pointerdown', event)
+	element.interactive = true
+	element.buttonMode = true
+}
